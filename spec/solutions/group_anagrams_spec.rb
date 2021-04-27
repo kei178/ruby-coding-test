dd8ec568b49f934635286a16c1e69d941848f0cd
kei178 <keisuke.inaba.cpa@gmail.com>
2021-04-27 08:17:52 -0500

    Add leetcode 49 group anagrams

---
+++ spec/solutions/group_anagrams_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require_relative '../../solutions/group_anagrams.rb'
+
+RSpec.describe 'group_anagrams' do
+  it 'returns [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]]' do
+    strs = ["eat","tea","tan","ate","nat","bat"]
+    expect(group_anagrams(strs)).to eq([["eat", "tea", "ate"], ["tan", "nat"], ["bat"]])  
+  end
+
+  it 'returns [[""]]' do
+    strs = [""]
+    expect(group_anagrams(strs)).to eq([[""]])  
+  end
+
+  it 'returns [["a"]]' do
+    strs = ["a"]
+    expect(group_anagrams(strs)).to eq([["a"]])  
+  end
+
+  it 'returns [["a"], [""]]' do
+    strs = ["a",""]
+    expect(group_anagrams(strs)).to eq([["a"], [""]])  
+  end
+end
